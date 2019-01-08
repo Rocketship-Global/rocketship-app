21d6c7690036e8b93a8b8f485a628a5cb2395f4a
Matthew <Matthew@DESKTOP-RD7MSLN>
2019-01-08 19:49:00 +1000

    deleted unwanted files

---
+++ features/config.py
@@ -0,0 +1,19 @@
+# USERNAME = 'followifyoulikeninja'
+# PASSWORD = 'ishan12345'
+# USER1 = 'teamninja'
+# USER2 = 'ninja'
+# BLACKLIST = ['user1', 'user2']
+
+
+# Kelsi
+# USERNAME = 'kelscece'
+# PASSWORD = 'ishan12345'
+# USER1 = 'alissaviolet'
+# USER2 = 'chanteljeffries'
+# BLACKLIST = ['user1', 'user2']
+
+USERNAME = 'theclosetau'
+PASSWORD = 'thecl0s3t'
+USER1 = 'theiconicau'
+USER2 = 'callithelabel'
+# BLACKLIST = ['user1', 'user2']
