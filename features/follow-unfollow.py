458cf888c7dbbeef794b3e4fa7b770a90b394f80
Matthew <Matthew@DESKTOP-RD7MSLN>
2019-01-09 18:52:00 +1000

    quota_supervisor to put hourly/daily to sleep. syntax error.

--- features/follow-unfollow.py
+++ features/follow-unfollow.py
@@ -9,10 +9,10 @@ session = InstaPy(username=USERNAME, password=PASSWORD) # headless_browser=True
 
 # let's go! :>
 with smart_run(session):
-    session.set_quota_supervisor(enabled = True, sleep_after = ["follows, unfollows"], sleepyhead = True, stochastic_flow = True, notify_me = True, peak_follows = (90, 900), peak_unfollows = (90, 900))
+    session.set_quota_supervisor(enabled = True, sleep_after = ["follows", "unfollows"], sleepyhead = True, stochastic_flow = True, notify_me = True, peak_follows = (90, 900), peak_unfollows = (90, 900))
     session.set_relationship_bounds(enabled = True, max_followers = 999999, max_following = 999999, min_followers = 5, min_following = 5)
     session.set_skip_users(skip_private=False, skip_no_profile_pic=True, skip_business=True)
 
     while True:
-        session.follow_likers ([USER1, USER2], photos_grab_amount = 2, follow_likers_per_photo = 225, randomize=False, sleep_delay=60, interact=False)
-        session.unfollow_users(amount=900, InstapyFollowed=(True, "nonfollowers"), style="FIFO", sleep_delay=60) # unfollow_after=86400
+        session.follow_likers ([USER1, USER2], photos_grab_amount = 2, follow_likers_per_photo = 225, randomize=False, sleep_delay=80, interact=False)
+        session.unfollow_users(amount=900, InstapyFollowed=(True, "nonfollowers"), style="FIFO", sleep_delay=80) # unfollow_after=86400
